1ec89d3fbbaefeb03fd670d5691e0f033320eb73
Çağlar ERGÜL <caglarergul@me.com>
2017-12-13 22:34:11 +0300

    Axios added.

---
+++ src/DAL/Database.js
@@ -0,0 +1,16 @@
+/**
+ * Created by caglarergul on 13.12.2017.
+ */
+import React from 'react';
+import axios from 'axios';
+
+
+
+const axios = (props, data) => (
+    return(
+      //axios.create("")
+        <div></div>
+    );
+)
+
+export default axios;
