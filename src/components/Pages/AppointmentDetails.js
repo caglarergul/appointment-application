46a521853064c1f23de87e1c8dab49f8ed2db893
Çağlar ERGÜL <caglarergul@me.com>
2017-12-26 20:14:17 +0300

    Get Appointment Details with Customer Details and add some action buttons.

--- src/components/Pages/AppointmentDetails.js
+++ src/components/Pages/AppointmentDetails.js
@@ -14,15 +14,52 @@ class AppointmentDetails extends Component {
         customerId: this.props.match.params.id,
         appointmentId: this.props.match.params.refid,
         customerList: [],
-        personDetail: []
+        personDetail: [],
+        appointmentList: [],
+        appointmentDetail: []
     };
 
 
     componentDidMount() {
        console.log(this.props.match.params.id);
         this.showExactCustomerHandler();
-
+        this.showExactAppointment();
     }
+
+    showExactAppointment = () => {
+        const itemsRef = firebase.database().ref('appointment');
+        itemsRef.on('value', (snapshot) => {
+            let items = snapshot.val();
+            let newState = [];
+            for (let item in items) {
+                newState.push({
+                    id: item,
+                    cus_id : items[item].id,
+                    date: items[item].date,
+                    isFirst: items[item].isFirst,
+                    isOnline: items[item].isOnline,
+                    isOver: items[item].isOver
+                });
+            }
+            this.setState({
+                appointmentList: newState
+            });
+            console.log("Appointment List: "+ this.state.appointmentList);
+            for (let i in this.state.appointmentList) {
+                console.log("customerId: "+ this.state.personDetail.id + " - cus_id: "+this.state.appointmentList[i].cus_id)
+
+                if (this.state.personDetail.id == this.state.appointmentList[i].cus_id) {
+                    this.setState({appointmentDetail: this.state.appointmentList[i]});
+                    console.log(this.state.appointmentDetail);
+                }else {
+                    console.log("bulamadık!");
+                }
+            }
+
+        });
+    };
+
+
     showExactCustomerHandler = () => {
         console.info("mounted");
         const itemsRef = firebase.database().ref('customer');
@@ -49,7 +86,6 @@ class AppointmentDetails extends Component {
                 customerList: newState
             });
             console.log(this.state.customerList);
-            debugger;
             for (let i in this.state.customerList) {
                 console.log("customerId: "+ this.state.customerId + " - cus_id: "+this.state.customerList[i].cus_id)
 
@@ -138,52 +174,34 @@ class AppointmentDetails extends Component {
                                 <table className="table table-striped">
                                     <tbody>
                                     <tr>
-                                        <th style={{width: '140px'}}>Firstname</th>
+                                        <th style={{width: '140px'}}>Date</th>
                                         <th style={{width: '30px'}}>:</th>
-                                        <td>{this.state.personDetail.firstName}</td>
+                                        <td>{this.state.appointmentDetail.date}</td>
                                     </tr>
                                     <tr>
-                                        <th>Surname</th>
+                                        <th>First Appointment?</th>
                                         <th>:</th>
-                                        <td>{this.state.personDetail.surname}</td>
+                                        <td>{this.state.appointmentDetail.isFirst}</td>
                                     </tr>
                                     <tr>
-                                        <th>Gender</th>
+                                        <th>Online Appointment?</th>
                                         <th>:</th>
-                                        <td>{this.state.personDetail.gender}</td>
+                                        <td>{this.state.appointmentDetail.isOnline}</td>
                                     </tr>
                                     <tr>
-                                        <th>Age</th>
+                                        <th>Appointment Finished?</th>
                                         <th>:</th>
-                                        <td>{this.state.personDetail.age}</td>
-                                    </tr>
-                                    <tr>
-                                        <th>Address</th>
-                                        <th>:</th>
-                                        <td>{this.state.personDetail.address}</td>
-                                    </tr>
-                                    <tr>
-                                        <th>Weight</th>
-                                        <th>:</th>
-                                        <td>{this.state.personDetail.weight}</td>
-                                    </tr>
-                                    <tr>
-                                        <th>Height</th>
-                                        <th>:</th>
-                                        <td>{this.state.personDetail.height}</td>
-                                    </tr>
-                                    <tr>
-                                        <th>Target Weight</th>
-                                        <th>:</th>
-                                        <td>{this.state.personDetail.targetWeight}</td>
-                                    </tr>
-                                    <tr>
-                                        <th>Disease</th>
-                                        <th>:</th>
-                                        <td>{this.state.personDetail.disease}</td>
+                                        <td>{this.state.appointmentDetail.isOver}</td>
                                     </tr>
                                     </tbody>
                                 </table>
+                            </div>
+                        </div>
+                        <div className="clearfix" style={{marginTop:"30px"}}>
+                            <div className="btn-group">
+                                <button className="btn btn-outline-success"><i className="fa fa-check"></i> Finish Appointment</button>
+                                <button className="btn btn-outline-primary"><i className="fa fa-edit"></i> Add Note</button>
+                                <button className="btn btn-outline-danger"><i className="fa fa-times"></i> Delete Appointment</button>
                             </div>
                         </div>
                     </div>
